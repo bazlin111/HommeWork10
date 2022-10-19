a7fbf159aa48594ee8f52fe7545d694bab5d367b
bazlin111 <pbazlin@yandex.ru>
2022-10-19 06:37:06 +0400

    ДЗ Базлин П.Н.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        Задача № 1
+        System.out.println();
+        String firstName = " Ivan";
+        String middleName = " Ivanovich";
+        String lastName = " Ivanov";
+        String fullName = " IVANOV IVAN IVANOVICH";
+        System.out.println(" ФИО сотрудника -" + lastName + firstName + middleName);
+        System.out.println();
+//        задача № 2
+        String tolowerFullName = fullName.toLowerCase();
+        System.out.println(" Данные ФИО для заполнения сотрудника -  " + tolowerFullName);
+//        Задача № 3
+        System.out.println();
+        String fullNameRus = "Иванов Семён Семёнович ";
+        fullNameRus = fullNameRus.replace("ё", "е");
+        System.out.println(" Данные ФИО сотрудника " + fullNameRus);
     }
 }
